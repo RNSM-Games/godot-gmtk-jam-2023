35f53063c4daa522ff27f8a4c62f9de475b1c118
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2023-07-07 21:20:43 -0400

    yes we did

--- scripts/Creature/Abilities/Dash.gd
+++ scripts/Creature/Abilities/Dash.gd
@@ -3,10 +3,16 @@ extends ActionItem
 func _init():
     cooldown = 7
     damage_modifier = 0
-    hitbox = get_node("CollisionShape2D")
+
+func _ready():
+    hitbox = get_parent().get_node("CharacterBody2D/Hitbox")
 
 
 
 func action():
     hitbox.disabled = true
-    
+    var character : CharacterBody2D = get_parent().get_node("CharacterBody2D")
+    var dash_pos : Vector2 = character.position + Vector2(10,10)
+    character.PlayerController.can_move = false
+    character.position = lerp(character.position,character.position + Vector2(50,50), .1)
+    hitbox.disabled = false
