6eea87623767edc756fed2e06699f6cfc23c38ac
Ioannis Kyriazis <ikyriazis@wpi.edu>
2023-07-07 21:40:37 -0400

    getting animation

--- scripts/Creature/Creatures/Orc.gd
+++ scripts/Creature/Creatures/Orc.gd
@@ -5,6 +5,7 @@ extends Creature
 
 @export var image : String
 
+
 func _init():
 	self.health = 50
 	self.damage = 1.0
@@ -15,9 +16,12 @@ func _ready():
 	sprite = get_node("CharacterBody2D/Sprite2D")
 	sprite.texture = load(image)
 	self.ability = get_node("Ability")
+	self.weapon = get_node("CharacterBody2D/MeleeWeapon")
 
 
 func _input(event):
+	if event.is_action_pressed("attack"):
+		weapon.action()
 	if event.is_action_pressed("Ability"):
 		print("HEllo")
 		ability.action()
