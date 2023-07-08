a879bd8519e1725e8811692d241911223e07b711
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2023-07-07 21:39:28 -0400

    Update Dash.gd

--- scripts/Creature/Abilities/Dash.gd
+++ scripts/Creature/Abilities/Dash.gd
@@ -1,18 +1,36 @@
 extends ActionItem
 
+var can_dash : bool = true
+var character : CharacterBody2D
+var is_dashing : bool = false
 func _init():
-    cooldown = 7
-    damage_modifier = 0
+	cooldown = 7
+	damage_modifier = 0
 
 func _ready():
-    hitbox = get_parent().get_node("CharacterBody2D/Hitbox")
+	hitbox = get_parent().get_node("CharacterBody2D/Hitbox")
+	character = get_parent().get_node("CharacterBody2D")
+
+
+func _physics_process(delta):
+	var dash_pos : Vector2 = Vector2.ZERO
+	if (!can_dash && !is_dashing):
+		dash_pos= character.position + Vector2(100,100)
+		is_dashing = true
+		hitbox.disabled = true
+		character.can_move = false
+	elif(is_dashing):
+		character.position = lerp(character.position,dash_pos, .1)
+	if(character.position == dash_pos):
+		is_dashing = false
+		hitbox.disabled = false
+	character.can_move = true
+	can_dash = true
+
+
+
 
 
 
 func action():
-    hitbox.disabled = true
-    var character : CharacterBody2D = get_parent().get_node("CharacterBody2D")
-    var dash_pos : Vector2 = character.position + Vector2(10,10)
-    character.PlayerController.can_move = false
-    character.position = lerp(character.position,character.position + Vector2(50,50), .1)
-    hitbox.disabled = false
+	can_dash = false
