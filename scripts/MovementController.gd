53e532083f9a1d0367eec7d00642e29cc7fa0da5
Ioannis Kyriazis <ikyriazis@wpi.edu>
2023-07-08 15:45:45 -0400

    fix main scene

--- scripts/MovementController.gd
+++ scripts/MovementController.gd
@@ -14,8 +14,6 @@ func move(speed: float, direction: Vector2):
 	position.y = clamp(position.y, 0, get_viewport_rect().size
 .y)
 	move_and_slide()
-    velocity = speed * direction
-    move_and_slide()
 
 
 func get_target() -> Vector2:
